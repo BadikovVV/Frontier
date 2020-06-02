a71dfb87a8d2a13267a4b0b5c29d70532756afd7
Бадиков Вячеслав <slv_b@mail.ru>
2020-06-02 17:38:03 +0400

    02-06-2020 в mainMenu добавлены ф-ции построения массива для меню по таблице в БД

--- mainMenu.php
+++ mainMenu.php
@@ -7,19 +7,50 @@ if (!defined('DBCONNECT'))
 //require_once(__ROOT__.'/vlg_header.php'); 
 /* */
 echo sprintf("<link  rel=\"stylesheet\" type=\"text/css\" href=\"css/mainMenu.css\">");
-$sqlStr="SELECT * FROM private_sector.menu";
-$result=qSQL($sqlStr);
-$menuArray=array();
-while($row=mysql_fetch_array($result)){
-	$pr=(int)$row["parent"];
-	$title=$row["title"];
-	$id=(int)$row["id"];
-	if (!isset($menuArray[$pr]))
-		$menuArray[$pr]=array();
-	$menuArray[$pr][]= array("id"=>$id,"title"=>$title);
+function getCat($mysqli){
+    $sqlStr="SELECT * FROM private_sector.menu";
+    $res=qSQL($sqlStr);
+    //Создаем масив где ключ массива является ID меню
+    $cat = array();
+    while($row = $res->fetch_assoc()){
+        $cat[$row['id']] = $row;
+    }
+    return $cat;
 }
+//Функция построения дерева из массива от Tommy Lacroix
+function getTree($dataset) {
+    $tree = array();
+    foreach ($dataset as $id => &$node) {   
+    //Если нет вложений
+        if (!$node['parent']){
+            $tree[$id] = &$node;
+        }else{
+        //Если есть потомки то перебераем массив
+            $dataset[$node['parent']]['childs'][$id] = &$node;
+        }
+    }
+    return $tree;
+}
+//Получаем подготовленный массив с данными
+$cat  = getCat($mysqli);
+//Создаем древовидное меню
+$tree = getTree($cat);
+
+
+
+//$sqlStr="SELECT * FROM private_sector.menu";
+//$result=qSQL($sqlStr);
+//$menuArray=array();
+//while($row=mysql_fetch_array($result)){
+//	$pr=(int)$row["parent"];
+//	$title=$row["title"];
+//	$id=(int)$row["id"];
+//	if (!isset($menuArray[$pr]))
+//		$menuArray[$pr]=array();
+//	$menuArray[$pr][]= array("id"=>$id,"title"=>$title);
+//}
 echo  "<pre>";
-print_r($menuArray);
+print_r($tree);
 echo  "</pre>";
 echo '<div id="container">';
 echo" <nav>";
